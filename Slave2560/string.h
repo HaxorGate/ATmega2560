a1cca823119ac54eabf92c236a1341d03bd3d70a
joelETA <joel@epicteamadventures.com>
2018-07-26 21:40:13 -0700

    working

--- Slave2560/string.h
+++ Slave2560/string.h
@@ -3,13 +3,60 @@
  *
  * Created: 7/26/2018 8:26:39 PM
  *  Author: ETAStaff
- */ 
+ */
 
 
 #ifndef STRING_H_
 #define STRING_H_
 
+class String {
+	public:
+		String( const char *cstr );
+		String( char *cstr );
+		String( const String& str );
+		String( int8_t s8 );
+		String( uint8_t u8 );
+		String( int16_t s16 );
+		String( uint16_t u16 );
+		String( int32_t s32 );
+		String( uint32_t u32 );
+		String( int64_t s64 );
+		String( uint64_t u64 );
+		String( float f32 );
+		operator const char*() const;
 
+		bool operator==( const String &str ) const;
+		bool operator!=( const String &str ) const;
+		bool operator==( int64_t num ) const;
+		bool operator==( int32_t num ) const;
+		bool operator==( int16_t num ) const;
+		bool operator==( int8_t num ) const;
+		bool operator==( uint64_t num ) const;
+		bool operator==( uint32_t num ) const;
+		bool operator==( uint16_t num ) const;
+		bool operator==( uint8_t num ) const;
+
+		size_t size( void ) const;
+		String operator+( const String &str ) const;
+		String operator+( const char *cstr ) const;
+
+		String &operator+=( const String &str );
+		String &operator+=( const char *cstr );
+		String &append( const char *cstr );
+		String &append( const String &str );
+		String &operator^( bool upper );
+		String &toUpper( void );
+		String &toLower( void );
+		String &remove( const char *cstr );
+		String &operator-=( const char *cstr );
+		String &remove( const String &str );
+		String &operator-=( const String &str );
+		String &prepend( const char *cstr );
+		String &prepend( const String &str );
+	protected:
+	private:
+		const char *_array;
+};
 
 
 
--- Slave2560/usart.h
+++ Slave2560/usart.h
@@ -5,11 +5,18 @@
  *  Author: ETAStaff
  */
 #include <avr/io.h>
+#include "string.h"
 
 #ifndef USART_H_
 #define USART_H_
 
-
+class USART {
+	USART( void );
+	void begin( uint32_t baud = 115200 );
+	void write( uint8_t data );
+	void write( uint16_t data );
+	void write( String data );
+};
 
 
 #endif /* USART_H_ */
